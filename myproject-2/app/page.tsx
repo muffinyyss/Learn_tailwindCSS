8be5635b75cede1645701a333387ebe5de05260c
muffinyyss <152370452+muffinyyss@users.noreply.github.com>
2025-08-20 21:46:13 +0700

    create myproject-2

---
+++ myproject-2/app/page.tsx
@@ -0,0 +1,10 @@
+import Image from "next/image";
+import styles from "./page.module.css";
+
+export default function Home() {
+  return (
+    <>
+      <h1 className="text-5xl text-center p-8 bg-pink-300 text-white">Hello eiei</h1>
+    </>
+  );
+}
