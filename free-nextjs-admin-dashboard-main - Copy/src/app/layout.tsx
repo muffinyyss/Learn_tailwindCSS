9454d3534073816f63ec2938e22b086112851147
muffinyyss <152370452+muffinyyss@users.noreply.github.com>
2025-08-21 16:51:27 +0700

    Login page not work

---
+++ free-nextjs-admin-dashboard-main - Copy/src/app/layout.tsx
@@ -0,0 +1,28 @@
+import { Outfit } from 'next/font/google';
+import './globals.css';
+
+import { SidebarProvider } from '@/context/SidebarContext';
+import { ThemeProvider } from '@/context/ThemeContext';
+import LoginPage from '@/components/auth/SignInForm'
+
+
+const outfit = Outfit({
+  subsets: ["latin"],
+});
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body className={`${outfit.className} dark:bg-gray-900`}>
+        {/* <ThemeProvider>
+          <SidebarProvider>{children}</SidebarProvider>
+        </ThemeProvider> */}
+        <LoginPage/>
+      </body>
+    </html>
+  );
+}
